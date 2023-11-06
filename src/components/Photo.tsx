6d2d56f03ce86d9fa281cd8289c53bcac52bc76c
chitwan001 <chitwan001@gmail.com>
2023-11-07 01:13:06 +0530

    clear garb and alt added to photos

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -3,7 +3,7 @@ import {PhotoData} from "../types/Response";
 export default function Photo({photoData}: { photoData: PhotoData }) {
     return (
         <div id={photoData.id} className={'grid place-self-center w-fit h-fit rounded-md overflow-hidden'}>
-            <img src={`https://live.staticflickr.com/${photoData.server}/${photoData.id}_${photoData.secret}_w.jpg`}/>
+            <img alt={photoData.title} src={`https://live.staticflickr.com/${photoData.server}/${photoData.id}_${photoData.secret}_w.jpg`}/>
         </div>
     )
 }
